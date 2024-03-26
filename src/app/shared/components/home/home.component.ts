4db8fec626cbdabead84932f53e679e39e64134a
tarunregmi <tarunregmi.io@gmail.com>
2024-03-26 14:08:43 +0545

    feat: add links to navigate my-orders

--- src/app/shared/components/home/home.component.ts
+++ src/app/shared/components/home/home.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OrderService } from '../../services/order.service';
 
 @Component({
   selector: 'qd-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  constructor(
+    public order_: OrderService,
+  ) {}
+
+  ngOnInit(): void {
+    this.order_.syncMyOrders();
+  }
+}
